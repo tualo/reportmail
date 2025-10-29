de726e901fb2c0b3095e375b86ac85ca50769d27
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2025-10-29 22:31:16 +0100

    Version bump to 1.0.2

--- src/Routes/Route.php
+++ src/Routes/Route.php
@@ -7,7 +7,7 @@ use Tualo\Office\Basic\Route as BasicRoute;
 use Tualo\Office\Basic\IRoute;
 use PHPMailer\PHPMailer\PHPMailer;
 
-class Route implements IRoute
+class Route extends \Tualo\Office\Basic\RouteWrapper
 {
     public static function register()
     {
